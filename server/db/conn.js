2c407f853e07087a0ced12250c54c9e5a849d26e
Vedang Shailesh Joshi <vedang2607@gmail.com>
2024-06-09 12:39:24 +0530

    default mongodb connection

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const db = "mongodb+srv://vedang:vedang123@cluster0.rxkvpjj.mongodb.net/ccmp?retryWrites=true&w=majority";
+const db = process.env.MONGODB_URL || "mongodb+srv://vedang:vedang123@cluster0.rxkvpjj.mongodb.net/ccmp?retryWrites=true&w=majority";
 mongoose.connect(db,{
     useUnifiedTopology: true,
     useNewUrlParser: true 
