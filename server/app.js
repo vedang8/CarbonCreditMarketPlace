8673d52c1ceff8d58e6b5af9fe35ca5435f16369
Vedang Shailesh Joshi <vedang2607@gmail.com>
2024-06-09 19:00:22 +0530

    Socket.io is configured

--- server/app.js
+++ server/app.js
@@ -2,9 +2,6 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
-const fileupload = require("express-fileupload");
-const morgan = require("morgan");
-const fs = require('fs');
 const socketIo = require("socket.io");
 const http = require("http");
 
@@ -17,19 +14,13 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
 
-// app.use(fileupload({
-//     useTempFiles: true,
-//     tempFileDir: "D:/Projects/CCMP/server/uploads",
-// }));
 const userRoute = require("./routes/userRoute");
 const creditsFormRoute = require("./routes/creditsFormRoute");
 const creditsRoute = require("./routes/creditsRoute");
 const sellCreditsRoute = require("./routes/sellCreditsRoute");
 const bidsRoute = require("./routes/bidsRoute");
-const notificationRoute = require("./routes/notificationRoute");
+const notificationRoute = require("./routes/notificationRoute")(io);
 
-const path = require("path");
-app.use(express.static(path.join(__dirname, "build")));
 app.use(userRoute);
 app.use(creditsFormRoute);
 app.use(creditsRoute);
